28351bfe53804f8293649895bf55a4b798886a9e
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-12-11 10:54:55 +0530

    trade router

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./mainRoutes";
 import cors from "cors"
 import { userRouter } from "./routes/authRouter";
+import { tradeRouter } from "./routes/tradeRouter";
 
 const app = express();
 app.use(express.json())
@@ -14,5 +15,6 @@ app.get("/", (req, res) => {
     res.send("Helloooooooooooooo")
 })
 app.use("/api/v1", userRouter)
+app.use("/api/v1/trade", tradeRouter)
 
 app.listen(port);
