802852565b816d2c506e9194ba47e5e11df79fa3
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-12-11 10:59:30 +0530

    file change

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import router from "./mainRoutes";
 import cors from "cors"
 import { userRouter } from "./routes/authRouter";
 import { tradeRouter } from "./routes/tradeRouter";
@@ -8,7 +7,6 @@ import { assetRouter } from "./routes/assetRouter";
 
 const app = express();
 app.use(express.json())
-app.use( "/api/v1", router)
 app.use(cors())
 
 const port = 3000;
--- apps/backend/src/routes/tradeRouter.ts
+++ apps/backend/src/routes/tradeRouter.ts
@@ -1,7 +1,19 @@
 import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
 
 export const tradeRouter = Router()
 
-tradeRouter.post("/", (req, res) => {
-    
+tradeRouter.post("/trade/create", (req, res) => {
+    const { asset, type, margin, leverage, slippage } = req.body
+
+    const orderId = uuidv4()
+
+    res.json({orderId})
+})
+
+tradeRouter.post("/trade/close", (req, res) => {
+    const { orderId } = req.body()
+
+    res.json({success: "Order closed"})
+
 })
