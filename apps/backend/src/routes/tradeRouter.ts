d45dda556af09b679eeae910a09eacb801069692
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-12-13 23:32:07 +0530

    open positions

--- apps/backend/src/routes/tradeRouter.ts
+++ apps/backend/src/routes/tradeRouter.ts
@@ -1,19 +1,82 @@
 import { Router } from "express";
 import { v4 as uuidv4 } from "uuid";
+import authMiddleware, { AuthRequest } from "../middleware";
+import { prismaClient as prisma } from "@repo/db/client";
 
 export const tradeRouter = Router()
 
-tradeRouter.post("/trade/create", (req, res) => {
-    const { asset, type, margin, leverage, slippage } = req.body
+const Assets = ["BTC", "SOL", "ETH"];
 
-    const orderId = uuidv4()
+tradeRouter.post("/create", authMiddleware, async (req: AuthRequest, res) => {
+    const { asset, type, margin, leverage, takeProfit, stopLoss } = req.body;
+    const userId = req.user.id;
 
-    res.json({orderId})
+    if (!Assets.includes(asset) || !(type === "LONG" || type === "SHORT")) {
+        return res.status(400).json({ error: "Please send correct parameters." })
+    }
+
+    if ((1 < leverage && leverage < 10) || margin < 1 || typeof leverage !== "number" || typeof margin !== "number") {
+        return res.status(400).json({ error: "Incorrect leverage or margin." })
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId
+        },
+        select: {
+            balance: true
+        }
+    })
+    if (!user || !user.balance) return res.status(400).json({ error: "User Not found." })
+
+    const userbalance = user.balance;
+    if (margin > userbalance) return res.status(400).json({ error: "Insufficient balance." })
+
+    let entryPrice = 10;  // To do: fetch price from websocket backend.
+
+    const positionSize = (margin * leverage) / entryPrice;
+    const liquidationPrice =
+        type === "LONG"
+            ? entryPrice * (1 - 1 / leverage)
+            : entryPrice * (1 + 1 / leverage);
+
+    const position = await prisma.$transaction(async (tx) => {
+        await tx.user.update({
+            where: {id: userId},
+            data: {
+                balance:{
+                    decrement: margin
+                }
+            }
+        })
+
+        return tx.position.create({
+            data: {
+                userId,
+                asset,
+                type,
+                margin,
+                leverage,
+                entryPrice,
+                positionSize,
+                liquidationPrice,
+                takeProfit,
+                stopLoss
+            }
+        })
+    })
+    
+    return res.status(201).json({
+        positionId: position.id,
+        entryPrice,
+        liquidationPrice,
+        positionSize
+    })
 })
 
-tradeRouter.post("/trade/close", (req, res) => {
+tradeRouter.post("/close", authMiddleware, (req: AuthRequest, res) => {
     const { orderId } = req.body()
 
-    res.json({success: "Order closed"})
+    res.json({ success: "Order closed" })
 
 })
