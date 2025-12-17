3cbdc6940d4298fc91456e3b07c1be0a74b6156c
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-12-17 15:28:21 +0530

    initialise redis in backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,19 +4,36 @@ import { userRouter } from "./routes/authRouter";
 import { tradeRouter } from "./routes/tradeRouter";
 import { candleRouter } from "./routes/candleRouter";
 import { assetRouter } from "./routes/assetRouter";
+import { createClient } from "redis";
 
 const app = express();
 app.use(express.json())
 app.use(cors())
 
 const port = 3000;
+const subscriber = createClient({ url: "redis://localhost:6379" })
 
-app.get("/", (req, res) => {
-    res.send("Helloooooooooooooo")
+async function startServer() {
+    try {
+        await subscriber.connect()
+
+        app.use("/api/v1", userRouter)
+        app.use("/api/v1/trade", tradeRouter)
+        app.use("/api/v1/candle", candleRouter)
+        app.use("/api/v1/asset", assetRouter)
+
+        app.listen(port, () => {
+            console.log(`Server listening on port ${port}`)
+        });
+    } catch (err) {
+        console.error("Failed to start server:", err)
+    }
+}
+
+process.on("SIGINT", async () => {
+    console.log("Shutting Down...");
+    await subscriber.quit()
+    process.exit(0)
 })
-app.use("/api/v1", userRouter)
-app.use("/api/v1/trade", tradeRouter)
-app.use("/api/v1/candle", candleRouter)
-app.use("/api/v1/asset", assetRouter)
 
-app.listen(port);
+startServer()
