531cbaf597e842f273eb5c3ac8fe457d38eb8ac3
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2025-12-25 22:46:15 +0530

    fetching redis data

--- apps/batchuploader/src/index.ts
+++ apps/batchuploader/src/index.ts
@@ -2,6 +2,21 @@ import { createClient } from "redis";
 
 const redisClient = createClient({ url: "redis://localhost:6379" });
 
+const STREAM_KEY = "price_stream";
+const GROUP_NAME = "price_uploaders";
+const CONSUMER_NAME = "worker-1";
+
+type RedisStreamMessage = {
+    id: string;
+    message: Record<string, string>;
+};
+
+type RedisStream = {
+    name: string;
+    messages: RedisStreamMessage[];
+};
+
+
 async function initRedis() {
     try {
         if (!redisClient.isOpen) {
@@ -13,13 +28,51 @@ async function initRedis() {
     }
 }
 
-async function getRedisData(){
+async function ensureConsumerGroup(){
+    try {
+        await redisClient.xGroupCreate(
+            STREAM_KEY, GROUP_NAME, "$", {MKSTREAM: true}
+        )
+        console.log("Consumer group created.")
+    } catch (err: any) {
+        if(err?.message?.includes("BUSYGROUP")){
+            console.log("Consumer group already exists.")
+        }else{
+            throw err;
+        }
+    }
+}
 
+async function getRedisData(){
+    await ensureConsumerGroup();
+    while(true){
+        const response = await redisClient.xReadGroup(
+            GROUP_NAME,
+            CONSUMER_NAME,
+            [{key: STREAM_KEY, id: ">"}],
+            {BLOCK:5000, COUNT:100}
+        )
+
+        if(!Array.isArray(response)) continue;
+
+        for(const rawStream of response){
+            if(typeof rawStream !== "object" || rawStream === null ){
+                continue;
+            }
+            const stream = rawStream as RedisStream
+            for(const msg of stream.messages){
+                const { market, price, decimals, timestamp } = msg.message
+                console.log("Recieved price", {market, price, decimals, timestamp})
+                
+                await redisClient.xAck(STREAM_KEY, GROUP_NAME, msg.id)
+            }
+        }
+    }
 }
 
 async function main() {
-    initRedis()
-    getRedisData()
+    await initRedis()
+    await getRedisData()
 }
 
 main()
