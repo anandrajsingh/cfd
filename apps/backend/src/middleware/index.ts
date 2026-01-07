23eaff024e44a5f798c9cc7aafd903180289c17a
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2026-01-07 19:08:54 +0530

    middleware updated and cors error resolved

--- apps/backend/src/middleware/index.ts
+++ apps/backend/src/middleware/index.ts
@@ -11,14 +11,9 @@ export interface AuthRequest extends Request {
 }
 
 export default function authMiddleware(req: AuthRequest, res: Response, next: NextFunction) {
-    const authHeader = req.headers["authorization"]
 
-    if (!authHeader || !authHeader.startsWith("Bearer")) {
-        return res.status(401).json({ error: "No token provided" })
-    }
-
-    const token = authHeader.split(" ")[1];
-
+    const token = req.cookies?.auth_token;
+    if(!token) return res.status(401).json({error: "Not Authorised."})
     try {
         const decoded = jwt.verify(token!, JWT_SECRET);
         req.user = decoded;
