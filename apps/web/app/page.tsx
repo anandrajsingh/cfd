b8181f1bfe35601c0e537c246f422df0ebfdd1f0
Anand Raj SinghDAD0-D779 <singhpariharanandraj@gmail.com>
2026-03-01 17:44:40 +0530

    landing update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,102 +1,67 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
+const data = [
+  { heading: "USERS", description: "1250+" },
+  { heading: "EXPERIENCE", description: "2008" },
+  { heading: "COUNTRY", description: "195+" },
+  { heading: "FEATURES", description: "30+" },
+]
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col">
+      <section className="relative h-screen bg-[url('/bg.webp')]">
+        <div className="relative max-w-400 mx-auto px-52 py-20 grid  gap-24">
+          <div className="max-w-5xl mx-auto">
+            <h1 className="text-8xl font-extrabold leading-tight text-teal-50">
+              BUILD PORTFOLIO
+              <br />
+              <span className="flex items-center gap-24">
+                <div className="h-full border border-teal-200 rounded-full">
+                  <img
+                    src="/space.jpg"
+                    alt="space"
+                    className="w-32 h-full p-1 rounded-full object-cover"
+                  />
+                </div>
+                AND THEN HOLD
+              </span>
+            </h1>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turborepo.com/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <div className="flex gap-12 ml-6">
+              <h1 className="text-8xl font-semibold text-transparent [-webkit-text-stroke:1px_#94A3B8]">
+                BUY OR SELL
+              </h1>
+
+              <button className="flex items-center gap-2 px-5 py-1 my-4 rounded-full border-2 border-orange-400/40 bg-orange-400/10 hover:bg-orange-400/20 transition">
+                <span className="text-orange-300 bg-orange-100 w-12 rounded-3xl">➜</span>
+                <span className="text-3xl font-medium text-orange-200">
+                  START
+                </span>
+              </button>
+            </div>
+          </div>
+
+          <div className="flex gap-10 px-16 bg-[#2C6E73] backdrop-blur-xl py-12 border border-white rounded-3xl p-6 shadow-2xl">
+            {data.map((d) => (
+              <div key={d.heading} className="w-full flex flex-col gap-4 text-teal-50">
+                <div className="h-0.5 w-full bg-teal-50" />
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-center gap-4">
+                    <div className="h-2 w-2 bg-teal-50 rounded" />
+                    <div className="">{d.heading}</div>
+                  </div>
+                  <div className="text-4xl font-semibold">{d.description}</div>
+                  <div className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</div>
+                </div>
+              </div>
+            ))}
+          </div>
+
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com/templates?search=turborepo&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://turborepo.com?utm_source=create-turbo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to turborepo.com →
-        </a>
-      </footer>
+      </section>
+      <section className="h-screen bg-yellow-50 flex">
+        <div className="w-1/2 h-full"></div>
+        <div className="w-1/2 h-full bg-yellow-100"></div>
+      </section>
     </div>
   );
 }
